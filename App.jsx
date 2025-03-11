f3b34fe209c940c57fe7db5a1f1bbd35b545dd61
Pooja-Mehra9921 <io6.poojamehra@gmail.com>
2025-03-11 17:47:50 +0530

    making a login page for employe management system

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// custom components
+import LoginPage from "./src/pages/Login";
+import SignUpPage from "./src/pages/SignUp";
+
+
+const App = ()=>{
+    return(
+        <>
+<BrowserRouter>
+<Routes>
+    <Route path="/" element={<LoginPage/>}/>
+    <Route path="/login" element={<LoginPage/>}/>
+    <Route path="/signup" element={<SignUpPage/>}/>
+</Routes>
+    </BrowserRouter>
+        </>
+    )
+};
+
+export default App;
