3a1a8f94acc28ef2669b16cfe7168a0cc2f3a4a6
Pooja-Mehra9921 <io6.poojamehra@gmail.com>
2025-03-20 17:57:37 +0530

    changes in reducer

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
-import taskManagementSystem from "../Redux/appReducer/appReducer"
+import taskManagementSystem from "./appReducer/appReducer"
 
 
 
