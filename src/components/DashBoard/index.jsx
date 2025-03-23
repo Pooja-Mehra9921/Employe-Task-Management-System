6ba712e48603b674440be03738a424459a46b76c
Pooja-Mehra9921 <159164676+Pooja-Mehra9921@users.noreply.github.com>
2025-03-23 23:15:08 +0530

    adding code

--- src/components/DashBoard/index.jsx
+++ src/components/DashBoard/index.jsx
@@ -3,14 +3,19 @@ import React from "react";
 import "./style.css"
 import { Box, Typography } from "@mui/material";
 import HomeCart from "../DashboardCart";
+import { useSelector } from "react-redux";
+
+
 
 const DashBoard = ()=>{
+
+    const employees = useSelector((store)=> store.app.employees)
     return(
         <>
         <Box className="css-1ygil4i-MuiToolbar-root"></Box>
         <Box className="homeSection-main-container">
-        <HomeCart title={"Total Department"} bgColor={"#3F51B5"} />
-        <HomeCart title={"Total Employees"} bgColor={"#1E88E5"} />
+        <HomeCart title={"Total Department"} bgColor={"#3F51B5"} numbers={4} />
+        <HomeCart title={"Total Employees"} bgColor={"#1E88E5"} numbers={employees.length} />
         <HomeCart title={"New Tasks"} bgColor={"#90CAF9"} />
         <HomeCart title={"Inprogress Task"} bgColor={"#64B5F6"} />
         <HomeCart title={"Completed Task"} bgColor={"#BBDEFB"} />
--- src/components/DashboardCart/index.jsx
+++ src/components/DashboardCart/index.jsx
@@ -4,12 +4,12 @@ import React from "react";
 import "./style.css"
 import { Box, Typography } from "@mui/material";
 
-const DashBoardCart =({title, bgColor})=>{
+const DashBoardCart =({title, bgColor, numbers})=>{
     return(
         <>
         <Box sx={{margin:"10px", backgroundColor: bgColor}} className="main-homecart-container">
             <Box>
-            <Typography sx={{textAlign:"center"}}>0</Typography>
+            <Typography sx={{textAlign:"center"}}>{numbers || "0"}</Typography>
             <Typography sx={{textAlign:"center"}}>{title}</Typography>
 
             </Box>
